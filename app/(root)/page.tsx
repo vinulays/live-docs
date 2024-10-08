dab90115b43c602846f2728b241170e1bfe5bf54
Vinula Senarathne <vinulapc@gmail.com>
2024-10-08 13:28:09 +0530

    feat: delete documents

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn";
+import { DeleteModal } from "@/components/DeleteModal";
 import Header from "@/components/Header";
-import { Button } from "@/components/ui/button";
+import Notifications from "@/components/Notifications";
 import { getDocuments } from "@/lib/actions/room.actions";
 import { dateConverter } from "@/lib/utils";
 import { SignedIn, UserButton } from "@clerk/nextjs";
@@ -23,7 +24,7 @@ const Home = async () => {
     <main className="home-container">
       <Header className="sticky left-0 top-0">
         <div className="flex items-center gap-2 lg:gap-4">
-          Notification
+          <Notifications />
           <SignedIn>
             <UserButton />
           </SignedIn>
@@ -62,6 +63,7 @@ const Home = async () => {
                   </div>
                 </Link>
                 {/* Add delete button */}
+                <DeleteModal roomId={id} />
               </li>
             ))}
           </ul>
